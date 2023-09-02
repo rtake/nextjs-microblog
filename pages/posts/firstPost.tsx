0da246d5ada4a9084e6b0f9479761ca635329974
rtake <rtake1110@gmail.com>
2023-09-02 20:47:58 +0900

    Headerを修正

--- pages/posts/firstPost.tsx
+++ pages/posts/firstPost.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Link from "next/link";
 
 const firstPost = () => {
   return (
     <div>
+      <Head>
+        <title>First post</title>
+      </Head>
       <h1>最初の投稿</h1>
       <Link href="/">
         <h2>ホームへ戻る</h2>
